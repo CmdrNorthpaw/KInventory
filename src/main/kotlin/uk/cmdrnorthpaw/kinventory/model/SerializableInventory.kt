cd0b3e1368cfaa081ddd1f1d49502d6bde0d7c18
CmdrNorthpaw <thetuxkid@gmail.com>
2021-04-10 12:25:13 +0100

    New SerializableInventory for polymorphism and consistency

---
+++ src/main/kotlin/uk/cmdrnorthpaw/kinventory/model/SerializableInventory.kt
@@ -0,0 +1,12 @@
+package uk.cmdrnorthpaw.kinventory.model
+
+import kotlinx.serialization.Serializable
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.inventory.Inventory
+import net.minecraft.item.ItemStack
+import uk.cmdrnorthpaw.kinventory.model.SerializableItemStack.Companion.serializable
+
+@Serializable
+abstract class SerializableInventory<T: Inventory>(val items: MutableList<SerializableItemStack>) {
+    abstract fun toInventory(): T
+}
