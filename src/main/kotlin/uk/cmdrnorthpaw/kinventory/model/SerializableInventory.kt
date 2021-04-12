3fcf4a3b297351e76938d573a15891d2af947283
CmdrNorthpaw <thetuxkid@gmail.com>
2021-04-12 18:01:37 +0100

    Document SerializableInventory.kt

--- src/main/kotlin/uk/cmdrnorthpaw/kinventory/model/SerializableInventory.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/kinventory/model/SerializableInventory.kt
@@ -6,7 +6,19 @@ import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import uk.cmdrnorthpaw.kinventory.model.SerializableItemStack.Companion.serializable
 
+/**
+ * Represents any [Inventory] that needs to be serialized.
+ * In its base form, currently supports inventories with [ItemStack]s in them
+ * Fluid support will be added when a fluid storage API is merged with Fabric
+ * @param [T]: The [Inventory] that this class represents.
+ * @property items: A [List] of [SerializableItemStack] that represents
+ * */
 @Serializable
 abstract class SerializableInventory<T: Inventory>(val items: List<SerializableItemStack>) {
+    /**
+     * Converts this [SerializableInventory] to an actual [Inventory] of type [T]
+     * For polymorphism reasons this has to be called without any parameters,
+     * so anything this inventory needs to be converted must be part of the class itself.
+     * */
     abstract fun toInventory(): T
 }
