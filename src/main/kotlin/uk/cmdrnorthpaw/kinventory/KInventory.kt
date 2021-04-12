9e7f2195de1d83e7d6f5c61df092b69cb5a4ad8e
CmdrNorthpaw <thetuxkid@gmail.com>
2021-04-12 09:22:44 +0100

    Forgot to register server start event

--- src/main/kotlin/uk/cmdrnorthpaw/kinventory/KInventory.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/kinventory/KInventory.kt
@@ -2,12 +2,11 @@ package uk.cmdrnorthpaw.kinventory
 
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import uk.cmdrnorthpaw.kinventory.inventory.player.SerializableServerPlayerInventory
 
 object KInventory : ModInitializer {
     override fun onInitialize() {
-        ServerLifecycleEvents.SERVER_STARTED.register({
-
-        })
+        ServerLifecycleEvents.SERVER_STARTED.register(SerializableServerPlayerInventory.Companion::onStart)
     }
 
 
