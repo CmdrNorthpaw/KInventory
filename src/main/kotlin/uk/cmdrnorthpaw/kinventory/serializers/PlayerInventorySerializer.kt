2e313ccacd6d88c79e6ea778967eed31c582a4b0
CmdrNorthpaw <thetuxkid@gmail.com>
2021-04-13 15:35:27 +0100

    Make PlayerInventorySerializer use InventorySurrogateSerializer

--- src/main/kotlin/uk/cmdrnorthpaw/kinventory/serializers/PlayerInventorySerializer.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/kinventory/serializers/PlayerInventorySerializer.kt
@@ -9,24 +9,15 @@ import net.minecraft.item.ItemStack
 import net.minecraft.util.registry.Registry
 import uk.cmdrnorthpaw.kinventory.inventory.SerializablePlayerInventory
 import uk.cmdrnorthpaw.kinventory.inventory.SerializablePlayerInventory.Companion.serializable
+import uk.cmdrnorthpaw.kinventory.model.InventorySurrogateSerializer
 
 /**
  * A convenience object that adds a serializer for [PlayerInventory].
  * Uses surrogate serialization and is basically identical to [SerializablePlayerInventory]
  * You can either invoke it directly or use the [serializer] extension function for PlayerInventory
  * */
-object PlayerInventorySerializer : KSerializer<PlayerInventory> {
-    override fun deserialize(decoder: Decoder): PlayerInventory {
-        return decoder.decodeSerializableValue(SerializablePlayerInventory.serializer()).toInventory()
-    }
-
-    override val descriptor: SerialDescriptor = SerializablePlayerInventory.serializer().descriptor
-
-    override fun serialize(encoder: Encoder, value: PlayerInventory) {
-        encoder.encodeSerializableValue(SerializablePlayerInventory.serializer(), value.serializable())
-    }
-
-    private fun getKey(stack: ItemStack) = Registry.ITEM.getKey(stack.item).toString()
-
-    fun PlayerInventory.serializer(): KSerializer<PlayerInventory> = PlayerInventorySerializer
+object PlayerInventorySerializer : InventorySurrogateSerializer<PlayerInventory, SerializablePlayerInventory>(
+    SerializablePlayerInventory.serializer(), SerializablePlayerInventory.Companion
+) {
+    fun PlayerInventory.serializer() = this@PlayerInventorySerializer
 }
