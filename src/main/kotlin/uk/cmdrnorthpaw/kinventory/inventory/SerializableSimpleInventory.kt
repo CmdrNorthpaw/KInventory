ae05160aae47b0ded7db56e37c0f068168d5cb54
CmdrNorthpaw <thetuxkid@gmail.com>
2021-04-14 20:37:32 +0100

    Use extension properties over mixins

--- src/main/kotlin/uk/cmdrnorthpaw/kinventory/inventory/SerializableSimpleInventory.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/kinventory/inventory/SerializableSimpleInventory.kt
@@ -2,10 +2,10 @@ package uk.cmdrnorthpaw.kinventory.inventory
 
 import kotlinx.serialization.Serializable
 import net.minecraft.inventory.SimpleInventory
-import uk.cmdrnorthpaw.kinventory.mixins.SimpleInventoryAccessor
 import uk.cmdrnorthpaw.kinventory.model.SerializableInventory
 import uk.cmdrnorthpaw.kinventory.model.SerializableItemStack
 import uk.cmdrnorthpaw.kinventory.model.SerializableItemStack.Companion.serializable
+import uk.cmdrnorthpaw.kinventory.utils.items
 
 @Serializable
 class SerializableSimpleInventory(
@@ -15,7 +15,7 @@ class SerializableSimpleInventory(
 
     companion object : SerializableInventoryCompanion<SimpleInventory, SerializableSimpleInventory> {
         override fun getSerializable(from: SimpleInventory): SerializableSimpleInventory {
-            val stacks = (from as SimpleInventoryAccessor).stacks
+            val stacks = from.items
             return SerializableSimpleInventory(stacks.toList().map { it.serializable() })
         }
 
