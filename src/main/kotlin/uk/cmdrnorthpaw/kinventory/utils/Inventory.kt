f45777f157067d8fb9e2837fb4024eebedaf9fa5
CmdrNorthpaw <thetuxkid@gmail.com>
2021-04-14 20:15:40 +0100

    Make Inventory.items an extension property instead of an extension function.

--- src/main/kotlin/uk/cmdrnorthpaw/kinventory/utils/Inventory.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/kinventory/utils/Inventory.kt
@@ -4,11 +4,12 @@ import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 
 /** Small function to get all the [ItemStack]s in an [Inventory] */
-fun Inventory.allItems(): List<ItemStack> {
-    val items = mutableListOf<ItemStack>()
+val Inventory.items: List<ItemStack>
+    get() {
+        val items = mutableListOf<ItemStack>()
 
-    for (slot in 0 until this.size()) {
-        items.add(slot, this.getStack(slot))
+        for (slot in 0 until this.size()) {
+            items.add(slot, this.getStack(slot))
+        }
+        return items
     }
-    return items
-}
